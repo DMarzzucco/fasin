de8f99b74d8cd4c7951c2b750ae36d4cf3a783bd
DMarzzucco <drrkermazyxokv@gmail.com>
2023-11-14 17:21:51 -0300

    update

---
+++ fs/script.js
@@ -0,0 +1,7 @@
+// navegacion
+const $hideShow=document.getElementById("hideShow"),
+      $aside=document.getElementById("aside");
+
+$hideShow.addEventListener("click",()=>{
+    $aside.classList.toggle("desplegar")
+});      
