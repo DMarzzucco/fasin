a153223b8a72e78690f0ea68626c40ade73ab11b
DMarzzucco <drrkermazyxokv@gmail.com>
2023-11-16 16:47:21 -0300

    update

--- fs/script.js
+++ fs/script.js
@@ -5,3 +5,12 @@ const $hideShow=document.getElementById("hideShow"),
 $hideShow.addEventListener("click",()=>{
     $aside.classList.toggle("desplegar")
 });      
+
+// tabs
+function openTab(tabName) {
+    var tabs = document.querySelectorAll('.tab');
+    tabs.forEach(function(tab) {
+        tab.classList.remove('active');
+    });
+    document.getElementById(tabName).classList.add('active');
+}
