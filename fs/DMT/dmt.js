7b02266c771236198f7e281ebdab3252973e0082
FrancoMazzei <126453133+FrancoMazzei@users.noreply.github.com>
2023-12-05 20:24:34 -0300

    Add files via upload

--- fs/DMT/dmt.js
+++ fs/DMT/dmt.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
+//ORIGINAL
+document.addEventListener('DOMContentLoaded', function () { 
     const titulos = document.querySelectorAll('.acordeon h3');
   
     titulos.forEach(titulo => {
@@ -24,3 +25,40 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
   
+
+//el culiao que solo se cierra cuando toco arriba
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const titulos = document.querySelectorAll('.acordeon h3');
+
+//   titulos.forEach(titulo => {
+//     titulo.addEventListener('click', function (event) {
+//       // Cierra todos los acordeones menos el abierto
+//       titulos.forEach(t => {
+//         if (t !== this) {
+//           t.classList.remove('active');
+//         }
+//       });
+
+//       // Abre o cierra el acordeón
+//       this.classList.toggle('active');
+//     });
+//   });
+
+//   // Cierra todos los acordeones cuando tocas afuera
+//   document.addEventListener('click', function () {
+//     titulos.forEach(t => t.classList.remove('active'));
+//   });
+
+//   // Evitar que el tocar dentro del acordeón cierre los demás acordeones
+//   document.querySelector('.acordeon').addEventListener('click', function (event) {
+//     event.stopPropagation();
+//   });
+
+//   // Permitir la selección de texto dentro del acordeón
+//   document.querySelectorAll('.acordeon .acor-content').forEach(content => {
+//     content.addEventListener('mousedown', function (event) {
+//       event.stopPropagation();
+//     });
+//   });
+// });
