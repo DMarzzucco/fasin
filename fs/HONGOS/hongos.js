226796303505d6226c3969004ed35ab5dffb8880
DMarzzucco <drrkermazyxokv@gmail.com>
2023-12-25 13:03:49 -0300

    update

---
+++ fs/HONGOS/hongos.js
@@ -0,0 +1,48 @@
+function openTab(tabName){
+    var tablas=document.querySelectorAll('.tab');
+    tablas.forEach(function(tab){
+        tab.classList.remove('active', 'escala-up');
+    });
+
+    document.getElementById(tabName).classList.add('active', 'escala-up');   
+}
+// precau
+var titulos = document.querySelectorAll('.titulos');
+
+// Iterar
+titulos.forEach(function(titulo) {
+    //  numero del título - del contenido
+    var num = titulo.id.replace('titulo', ''); // número de id
+
+    //  event  click
+    titulo.addEventListener('click', function() {
+        var contenido = document.getElementById('conten' + num);
+        
+        // animatk
+        if (contenido.style.display === 'none' || contenido.style.display === '') {
+            contenido.style.display = 'block';
+            contenido.classList.add('escala-up');
+        } else {
+            contenido.style.display = 'none';
+        }
+    });
+    
+    // event hide
+    var contenido = document.getElementById('conten' + num);
+    contenido.addEventListener('click', function(event) {
+        // none title
+        event.stopPropagation();
+
+        // Oculta el contenido
+        contenido.style.display = 'none';
+        contenido.classList.remove='escala-up'
+    });
+});
+// h & s
+function hideShow(){
+    var ifectSection= document.querySelector('.ifect');
+    // block
+    ifectSection.style.display ='block';
+    //redireccion
+    window.location.href = window.location.href.split('#')[0] + '#ifect';
+};
