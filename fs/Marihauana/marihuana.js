91a41f5a0954f1d1a1e2d1b4b8225fdecd4b091e
FrancoMazzei <126453133+FrancoMazzei@users.noreply.github.com>
2024-01-11 15:42:30 -0300

    Add files via upload

--- fs/Marihauana/marihuana.js
+++ fs/Marihauana/marihuana.js
@@ -39,3 +39,17 @@ document.getElementById("hideShow-res").addEventListener("click", function () {
   var asideRespon = document.getElementById("aside-respon");
   asideRespon.style.display = (asideRespon.style.display === "none" || asideRespon.style.display === "") ? "block" : "none";
 });
+
+
+
+//acordeón responsive consumo
+document.addEventListener('DOMContentLoaded', function () {
+  var accordionTitles = document.querySelectorAll('.accordion-item-title');
+
+  accordionTitles.forEach(function (title) {
+      title.addEventListener('click', function () {
+          var content = this.nextElementSibling;
+          content.style.display = content.style.display === 'block' ? 'none' : 'block';
+      });
+  });
+});
