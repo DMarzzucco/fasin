bc25e778d0904711c95b7892c10e19b67468b83c
DMarzzucco <drrkermazyxokv@gmail.com>
2024-01-19 17:05:29 -0300

    last

--- fs/LSD/adult.js
+++ fs/LSD/adult.js
@@ -1,16 +1,42 @@
+// respon
+let menuVisible = false;
+function showAndHideM() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    } else {
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+function selecionar() {
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
 
 // tabla
-function openTab(tabName){
-    var tablas=document.querySelectorAll('.tab');
-    tablas.forEach(function(tab){
+function openTab(tabName) {
+    var tablas = document.querySelectorAll('.tab');
+    tablas.forEach(function (tab) {
         tab.classList.remove('active', 'scale-up-center');
     });
 
-    document.getElementById(tabName).classList.add('active', 'scale-up-center');   
+    document.getElementById(tabName).classList.add('active', 'scale-up-center');
 }
 
+// respon-tab
+function opentb(tbName) {
+    var tblas = document.querySelectorAll('.tb');
+    tblas.forEach(function (tb) {
+        tb.classList.remove('active', 'scale-up-center');
+    });
+
+    document.getElementById(tbName).classList.add('active', 'scale-up-center');
+}
+
+
 // red
-function redireccionar (url){
+function redireccionar(url) {
     if (url && url !== '#') {
         window.location.href = url;
     }
